8c9589ac37f20acd39f94bc77c15096e5da8156a
alexou2 <alexandreValin2@gmail.com>
2023-04-25 16:32:56 -0400

    added file reDING

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 #![allow(unused)]
 // use Reqwest;
-// use std::fs;
+use std::fs;
 use std::env;
 
 fn main() {
-    println!("Hello, world!");
-    let osx = env::consts::OS;
-    println!("The host is running {}", osx)
+    println!("The host is running {}", env::consts::OS);
+
+    let mut source_code = fs::read_to_string("src/main.rs");
+    println!("code: {:?}", source_code)
 }
