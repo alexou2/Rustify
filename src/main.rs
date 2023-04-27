62b18f5f4c67bfa123c4c2172028ce64a45a8437
alexou2 <alexandreValin2@gmail.com>
2023-04-27 15:31:17 -0400

    basic implementation of search

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,20 @@ use reqwest::blocking::get;
 use serde_json::json;
 use serde_json::to_string_pretty;
 use serde_json::Value;
-mod spotify;
 mod auth;
+mod spotify;
 
 fn main() -> Result<(), reqwest::Error> {
     println!("The host is running {}", env::consts::OS);
-// println!("{}", to_string_pretty(&spotify::login()).unwrap());
+    // println!("{}", to_string_pretty(&spotify::login()).unwrap());
     // println!("{:?}", spotify::search("lol", "track").unwrap());
     let mut access_token = auth::auth().unwrap();
-
+    type_of(&access_token);
+    spotify::search("lol", "artist", access_token);
     Ok(())
 }
 
-fn print_type_of<T>(_: &T) -> String {
-    format!("{}", std::any::type_name::<T>())
+fn type_of<T>(_: &T){
+    let type_of_var =  format!("{}", std::any::type_name::<T>());
+    println!("{}", type_of_var)
 }
--- src/spotify.rs
+++ src/spotify.rs
@@ -1,21 +1,29 @@
+use reqwest::blocking::Client;
+use reqwest::header::HeaderMap;
 use reqwest::{blocking, header::EXPECT};
-use serde_json::Value;
 use serde_json::json;
+use serde_json::Value;
+use std::collections::HashMap;
 
 const BASE_URL: &str = "https://api.spotify.com/v1/";
-pub fn search(query: &str, search_type: &str) -> Result<Value, Box<dyn std::error::Error>> {
+
+pub fn search(query: &str, search_type: &str, access_token: String) -> Result<Value, Box<dyn std::error::Error>> {
     // default values
     let method = "search";
     let mut q = query.replace(" ", "%20");
     let mut request_url = format!("{}{}?query={}&type={}", BASE_URL, method, q, search_type);
-
-    // let access_token = login();
+    let formatted_access_token = format!("Bearer {}", access_token);
 
     // making the get reqwest
-    let response = reqwest::blocking::get(&request_url)?;
-    let json_data = response.json::<serde_json::Value>()?;
-    println!("{:?}", request_url);
+    let client = reqwest::blocking::Client::new();
+    let res = client.get(request_url)
+    .header("Authorization", formatted_access_token)
+    .send()?;
+    // .headers("Authorization: Bearer","BQCIhY-r5P1wlhEawKthWQ4Y1nz9aJfbXEdR0p-OouOrpcNHpLU9Xe4zm2KM3FckX90HutEUBORrfnKfpe4Tp8uXO0cV662B8qfRLh4C1VaXBxvnhwJ3")
+    // .await
+    // ?;
+
+    let json_data = res.json::<serde_json::Value>()?;
+    println!("{:?}", json_data);
     return Ok(json_data);
 }
-
-
