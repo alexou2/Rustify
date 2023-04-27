187825a2ab820027d9ed687562d9d5ef560a11bb
alexou2 <alexandreValin2@gmail.com>
2023-04-27 10:47:29 -0400

    started getting access token grom json

--- src/main.rs
+++ src/main.rs
@@ -4,35 +4,17 @@ use std::fs;
 // use reqwest::Error;
 use reqwest::blocking::get;
 use serde_json::json;
+use serde_json::to_string_pretty;
 use serde_json::Value;
 mod spotify;
 
-fn main() -> Result<(), reqwest::Error>  {
+fn main() -> Result<(), reqwest::Error> {
     println!("The host is running {}", env::consts::OS);
-
-//     let response = reqwest::blocking::get("https://api.mangadex.org/manga")?;
-    
-//     let json_data = response.json::<serde_json::Value>()?;
-  
-// println!("{}", json_data["data"][0]["attributes"]);
-
-
-println!("{:?}", spotify::search("lol", "song").unwrap());
-Ok(())
+println!("{}", to_string_pretty(&spotify::login()).unwrap());
+    // println!("{:?}", spotify::search("lol", "track").unwrap());
+    Ok(())
 }
-
-
 
 fn print_type_of<T>(_: &T) -> String {
     format!("{}", std::any::type_name::<T>())
-}
-
-
-
-fn fetch() -> Result<(), Box<dyn std::error::Error>> {
-    let response = get("https://api.mangadex.org/manga")?.text()?;
-    let json_response = json!(response);
-
-    println!("json{}", json_response[0]);
-    Ok(())
 }
--- src/spotify.rs
+++ src/spotify.rs
@@ -1,14 +1,34 @@
-use reqwest::blocking;
+use reqwest::{blocking, header::EXPECT};
 use serde_json::Value;
+use serde_json::json;
 
-pub fn search(query: &str, search_type: &str) -> Result<String, Box<dyn std::error::Error>> {
+const BASE_URL: &str = "https://api.spotify.com/v1/";
+pub fn search(query: &str, search_type: &str) -> Result<Value, Box<dyn std::error::Error>> {
     // default values
-    const BASE_URL: &str = "https://api.spotify.com/v1/search";
+    let method = "search";
     let mut q = query.replace(" ", "%20");
+    let mut request_url = format!("{}{}?query={}&type={}", BASE_URL, method, q, search_type);
+
+    // let access_token = login();
 
     // making the get reqwest
-    let response = reqwest::blocking::get(BASE_URL)?;
+    let response = reqwest::blocking::get(&request_url)?;
     let json_data = response.json::<serde_json::Value>()?;
-    println!("{}", q);
-    return Ok(q);
+    println!("{:?}", request_url);
+    return Ok(json_data);
 }
+
+pub fn login() -> &'static str {
+    const LOGIN_URL: &str = "https://accounts.spotify.com/api/token";
+    const CONTENT_TYPE:&str = "Content-Type: application/x-www-form-urlencoded";
+    let mut acces_token: &str = "";
+    let client = reqwest::blocking::Client::new();
+    let mut response = client.post(LOGIN_URL).body("abc").send().expect("err");
+    let json_data = response.json::<serde_json::Value>().expect("msg");
+    print!("{:?}",json_data["url"]);
+    return "acces_token";
+}
+
+// curl -X POST "https://accounts.spotify.com/api/token" \
+//      -H "Content-Type: application/x-www-form-urlencoded" \
+//      -d "grant_type=client_credentials&client_id=0d65fded6a3842cc88530433b728f436&client_secret=98695a47a9ba449ca1b6f709c93762d2"
